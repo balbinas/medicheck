86eb90f43239340c62e9a6e703413c89aedd22f5
Balbina <balbinasantana@gmail.com>
2016-01-07 18:39:28 -0600

    fixing in code to work

---
+++ Medicheck/Paciente.h
@@ -0,0 +1,23 @@
+//
+//  Paciente.h
+//  Medicheck
+//
+//  Created by Balbina Santana on 1/7/16.
+//  Copyright © 2016 Balbina Santana. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Paciente : NSObject
+
+@property(nonatomic,strong) NSString *nombre;
+@property(nonatomic,strong) NSString *apellido;
+@property(nonatomic,strong) NSString *usuario;
+@property(nonatomic,strong) NSString *contrasena;
+@property NSInteger telefono;
+@property NSDate *dob;
+@property (nonatomic, strong) NSString *sexo;
+
+- (id) initWithNombre:(NSString*)nom apellido: (NSString*) apel telefono: (NSInteger) tel usuario: (NSString*) user contrasena: (NSString*) pssw dob: (NSDate*) dob sexo: (NSString*) sex;
+
+@end
---
+++ Medicheck/PacienteAgregarViewController.m
@@ -0,0 +1,53 @@
+//
+//  PacienteAgregarViewController.m
+//  Medicheck
+//
+//  Created by Balbina Santana on 1/7/16.
+//  Copyright © 2016 Balbina Santana. All rights reserved.
+//
+
+#import "PacienteAgregarViewController.h"
+
+@interface PacienteAgregarViewController ()
+
+@end
+
+@implementation PacienteAgregarViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)btAgregar:(id)sender {
+    if(![self.tfNombre.text isEqualToString:@""])
+    {
+        NSString *nom = self.tfNombre.text;
+        NSString *apel = self.tfApellido.text;
+        NSString *user = self.tfUsuario.text;
+        NSString *pssw = self.tfContrasena.text;
+        NSString *sex = self.segSex.segmentedControlStyle;
+        NSInteger tel = [self.tfTelefono.text integerValue];
+        
+        
+        [self.delegado agregaPaciente: withApellido: apel withTelefono: withUsuario: withContrasena: withSexo: withDob: withTelefono:tel];
+        
+        [self.delegado quitaVista];
+    }
+}
+@end
